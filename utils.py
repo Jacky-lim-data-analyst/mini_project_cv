1ece5adbe1601b6fbc9d73db5de64c788b2d0699
Jacky-lim-data-analyst <84822819+Jacky-lim-data-analyst@users.noreply.github.com>
2024-07-18 11:49:35 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from urllib.request import urlretrieve
 import cv2 as cv
 import matplotlib.pyplot as plt
+import numpy as np
 
 def download_save_img(url, filename):
     """Function to download image and save the image as filename
@@ -33,18 +34,23 @@ def display_image(window_name, image, adjust=False):
     cv.waitKey(0)
     cv.destroyAllWindows()
 
-def display_images(img_list, titles):
+def display_images(img_list, titles, adjust=False):
     """Function to display multiple images
     Arguments:
     ---
     img_list: list of images
     titles: list of titles
+    adjust: Boolean that triggers cv.WINDOW_NORMAL
     
     Return:
     ---
     One or more display windows"""
     for img, title in zip(img_list, titles):
-        cv.imshow(title, img)
+        if adjust:
+            cv.namedWindow(title, cv.WINDOW_NORMAL)
+            cv.imshow(title, img)
+        else:
+            cv.imshow(title, img)
 
     cv.waitKey(0)
     cv.destroyAllWindows()
@@ -78,3 +84,61 @@ def matplotlib_show_images(arr_list, nrow, ncol, titles=[], figsize=(10, 10), ax
             plt.axis("off")
 
     plt.show()
+
+def point_op(img, alpha, beta):
+    """Pixel transform function
+    Arguments:
+    ---
+    img: source image (uint8)
+    alpha: coefficient
+    beta: bias
+    
+    Returns:
+    ---
+    image (uint8)"""
+    img = img.astype("float32")
+    res = alpha * img + beta
+    res = np.clip(res, 0, 255)
+    return np.uint8(res)
+
+# gamma correction
+def gamma_correction(img, gamma=1):
+    """Gamma correction function
+    Arguments:
+    ---
+    img: source image (uint8)
+    gamma: 1 (default)
+    
+    Returns:
+    ---
+    image (uint8)"""
+    lookUpTable = np.empty((1, 256), dtype=np.uint8)
+    for i in range(256):
+        lookUpTable[0, i] = np.clip(pow(i / 255, gamma) * 255.0, 0, 255)
+    return cv.LUT(img, lookUpTable)
+
+# automatic Canny edge detector
+def auto_canny(img, method=None, sigma=0.33):
+    """Automatic Canny edge detection
+    img: grayscale image (uint8)
+    methods: otsu, triangle and median (string)
+    sigma: default: 0.33
+    
+    Returns:
+    ---
+    8-bit single channel image"""
+    if method == "median":
+        retVal = np.median(img)
+    
+    elif method == "triangle":
+        retVal = cv.threshold(img, 0, 255, cv.THRESH_TRIANGLE)[0]
+    
+    elif method == "otsu":
+        retVal = cv.threshold(img, 0, 255, cv.THRESH_OTSU)[0]
+    
+    else:
+        raise Exception("method specified not available")
+    
+    lowTh = (1 - sigma) * retVal
+    highTh = (1 + sigma) * retVal
+    return cv.Canny(img, lowTh, highTh)
