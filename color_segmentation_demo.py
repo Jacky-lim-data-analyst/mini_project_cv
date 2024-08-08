de9a0ab8c1443ff85c0f811780ce951b32fda8f6
Jacky-lim-data-analyst <84822819+Jacky-lim-data-analyst@users.noreply.github.com>
2024-08-08 09:59:06 +0800

    Add files via upload
    Add docstring to function

--- color_segmentation_demo.py
+++ color_segmentation_demo.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import argparse
 import numpy as np
 from utils import display_images
+import os
 
 ap = argparse.ArgumentParser()
 # ap.add_argument("-img", "--imgPath", type=str, help="path of source image")
@@ -11,27 +12,40 @@ args = vars(ap.parse_args())
 
 def color_segment(img: np.ndarray, low_col: tuple, high_col: tuple, smooth: bool = True, 
                   morphology: bool = True) -> np.ndarray:
+    """Color based segmentation
+    Arguments:
+    ---
+    img: BGR image (uint8)
+    low_col, high_col: HSV tuple (low and high thresholds)
+    smooth: Gaussian smoothing (default: True)
+    morphology: morphological operation (default: True)
+    
+    Returns:
+    ---
+    Segmented image (uint8)"""
     img_copy = img.copy()
 
     if smooth:
         img = cv.GaussianBlur(img, (5, 5), 0)
 
-    img_hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
+    img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     mask = cv.inRange(img_hsv, low_col, high_col)
     # morphological operation
     if morphology:
-        opened = cv.morphologyEx(mask, cv.MORPH_OPEN, None)
-    return cv.bitwise_and(img_copy, img_copy, mask=opened)
+        mask = cv.morphologyEx(mask, cv.MORPH_OPEN, None)
+    return cv.bitwise_and(img_copy, img_copy, mask=mask)
 
 # imgs = []
 blue_low = tuple(args["blueLow"])
 blue_high = tuple(args["blueHigh"])
+save_dir = "./tmp/"
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
 for i in range(1, 6):
     img_pant = cv.imread(cv.samples.findFile(f"images/color_spaces/pant{i}.jfif"))
-    img_pant_rgb = cv.cvtColor(img_pant, cv.COLOR_BGR2RGB)
-    res = color_segment(img_pant_rgb, blue_low, blue_high)
-    res = cv.cvtColor(res, cv.COLOR_RGB2BGR)
+    # img_pant_rgb = cv.cvtColor(img_pant, cv.COLOR_BGR2RGB)
+    res = color_segment(img_pant, blue_low, blue_high)
+    # res = cv.cvtColor(res, cv.COLOR_RGB2BGR)
     display_images([img_pant, res], ("original", "segment"))
     cv.imwrite(f"tmp/res{i}.jpg", res)
-
-    
